09a8dce4e5f742aea01e2dd19348cd8f68a5b5bb
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-06-07 12:27:58 +0530

    Add files via upload

---
+++ 1st Program -HelloWorld.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+int  main()//function declarator
+{ //begin of function 
+    std :: cout << "Hello World";// Prints Hello World
+    return 0;  
+} // end of function
+
+//std is the name of the namespace and cout is under the scope.
---
+++ 2nd Program -Hello World.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+int  main(){
+    using namespace std;
+     cout << "Hello World";// Prints Hello World
+    return 0;  
+}
+
+//Using namespace std means that you don't have to write std::cout and we can use the narrower scope namespace at any place. 
