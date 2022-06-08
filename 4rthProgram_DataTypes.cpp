dc776b42791d6bb4fe8a817b5858789d9ea1d58a
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-06-08 13:01:53 +0530

    Add files via upload

--- 4rthProgram_DataTypes.cpp
+++ 4rthProgram_DataTypes.cpp
@@ -5,7 +5,12 @@ int main()
     using namespace std;
     //Primitive Data Types
     int age = 26;//Integer value
-    double distance = 12.6 ; //floating point value
+    float floating_val1 = 1.0f;//Float value
+    float floating_val2= 1.5F;//Float value
+    float floating_val3 = 1.0;//Float value
+    double distance = 12.6D ; //floating point value
+    double distance1 = 12.67d ; //floating point value
+    double distance2 = 12.672e4; //floating point value
     char grade = 'A'; //character value
     string str ; //String value under String header 
     bool ok = false; //takes 
@@ -18,7 +23,12 @@ int main()
     cout << "Your surname is : " << name << "\n" ; 
     cout << "Age is:" << age << "\n" ;
     cout << "Distance is:" << distance << "\n" ;
+    cout << "Distance 1 is:" << distance1 << "\n" ;
+    cout << "Distance 2 is:" << distance2 << "\n" ;
     cout << "Is your age < 25?:"<< ok << "\n";
+    cout<< "floating_val1 =" << floating_val1<<"\n";
+    cout<< "floating_val2 =" << floating_val2<<"\n";
+    cout<< "floating_val3 =" << floating_val3<<"\n";
     return 0;
 
 
