c2c45e6dbd418df66bad65380b7bb0c720eefe8d
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-06-08 14:11:24 +0530

    Add files via upload

---
+++ 6rthProgram_GlobalAndLocal.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+int num = 20; //global variable
+int main(){
+using namespace std;
+int sum = 20 + num; // sum is local variable
+cout << num << "\n" ;
+cout << sum << "\n" ;
+
+}
