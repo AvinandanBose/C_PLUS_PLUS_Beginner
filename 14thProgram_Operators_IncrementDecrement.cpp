1abb409f9f45f18553ad072573b64f2812afd57f
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-06-17 11:44:59 +0530

    Add files via upload

---
+++ 14thProgram_Operators_IncrementDecrement.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+using namespace std;
+int main(){
+    //Post Increment Operation
+    cout<<"Post Increment Operation"<<"\n";
+    int a = 10;
+    int c =  a++;
+    cout<<"A = "<<a<<"\n";
+    cout<<"C = "<<c<<"\n";
+    //Post Increment Operation
+    cout<<"Pre Increment Operation"<<"\n";
+    int d = 10;
+    int e =  ++d;
+    cout<<"D = "<<d<<"\n";
+    cout<<"E = "<<e<<"\n";
+}
