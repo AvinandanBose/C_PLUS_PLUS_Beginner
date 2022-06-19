a489465c2188d33f02e0070c9d74f2b47b676139
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-06-19 15:13:35 +0530

    Add files via upload

---
+++ 21thProgram_Operators_BitwiseNOTOperator.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    bool s = false;
+    bool t = ~s;
+    cout<<t<<"\n";
+    return 0;
+}
+
+/*************
+ * s = false = 0;
+ * t = ~s = true =1;
+ * 
+ * 
+ * 
+ * 
+ * ********/
