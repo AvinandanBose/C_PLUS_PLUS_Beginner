579f8bbeb6d11d7fc0d3b2483a9a0e2d0e8f50bd
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-06-22 08:52:35 +0530

    Add files via upload

---
+++ 22thProgram_SpecialOperators_Comma.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int j;
+   int i = (j=3, j+2);
+   cout << "i=" << i << "\n";
+   
+   for (int a =0 , b =0 ;a <=12; a++,b++){
+    cout <<  "a=" << a << "\n";
+    cout << "b =" << b << "\n";
+
+   }
+
+}
