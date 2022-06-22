099b114be9cf3e23f66ebc61c7d516b10deae5a6
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-06-22 17:15:31 +0530

    Add files via upload

--- 26thProgram_TypeDefStatement.cpp
+++ 26thProgram_TypeDefStatement.cpp
@@ -6,10 +6,13 @@ int main() {
     typedef int Avinandan ; 
     typedef char Myname;
     typedef double price;
+    typedef unsigned long ulong;
+    ulong costing = 12;
     Avinandan age = 18;
     price Price = 19.50;
     Myname name[80] = "Avinandan";
-    cout << age << " " << name << endl;
-    cout << Price<< endl;
+    cout <<"age=" <<age << " " <<"name=" <<name << endl;
+    cout <<"Price = " <<Price<< endl;
+    cout <<"costing="<< costing<< endl;
     return 0;
 }
