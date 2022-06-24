672c2477e68654e622fc78de2658abeaa760ad1f
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-06-24 10:49:50 +0530

    Add files via upload

---
+++ 27thProgram_Constants_FloatingPointConstants.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //Decimal Notation
+    float a  = 0.976 - 0.71 / 0.5; 
+    double b  =  0.975 - 0.712 / 0.53; 
+    cout << a << endl;
+    cout << b << endl;
+
+    //Exponential Notation
+    float c  = 3.4E4;
+    double d  = 3e8;
+    cout << c << endl;
+    cout << d << endl;
+
+    return 0;
+
+
+}
