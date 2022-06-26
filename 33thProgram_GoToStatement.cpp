ce9b374699fc2dacd6d517243b0b8018073b57f1
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-06-26 17:06:48 +0530

    Add files via upload

---
+++ 33thProgram_GoToStatement.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    MyName: //Label for goto statement
+    cout<<"Enter your age:";
+    cin>>age;
+    if(age<0){
+        cout<<"Invalid age"<<"\n";
+        goto MyName;
+    }
+    else if(age<18){
+        cout<<"You are not eligible to vote"<<"\n";
+    }
+    else{
+        cout<<"You are eligible to vote"<<"\n";
+    }
+
+    goto MyName;
+    return 0;
+}
