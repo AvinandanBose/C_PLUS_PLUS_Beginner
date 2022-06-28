37dd85ad2a05ba56eb442239fb09755bd626da4c
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-06-28 19:03:55 +0530

    Add files via upload

---
+++ 38thProgram_DoWhileStatement.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i=0;// assignment
+    do{
+    
+        cout<<i<<endl;
+        i++; //updation
+        }while(i<5);
+    return 0;
+}
