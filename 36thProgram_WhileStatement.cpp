2b4727e27c680302e805561cb72a3be15b285d5e
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-06-28 17:29:12 +0530

    Add files via upload

---
+++ 36thProgram_WhileStatement.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=0; // initialization
+    while(i<=10) //condition
+    {
+        cout<<i<<endl;
+        i++; //updation
+    }
+    return 0;
+}
