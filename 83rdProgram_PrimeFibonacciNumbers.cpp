0a19fe9277b8e763bfc72bb7e1ee05d6c0c3e1c3
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-07-09 07:28:22 +0530

    Add files via upload

---
+++ 83rdProgram_PrimeFibonacciNumbers.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int range ;
+    cout << "Enter the range : ";
+    cin >> range;
+    int firstTerm = 0;
+    int secondTerm = 1;
+    for (int i=1; i<=range;i++){
+        int thirdTerm = firstTerm + secondTerm;
+        int s =0;
+        for(int j=2; j<=thirdTerm/2; j++){
+            if(thirdTerm%j==0){
+                s = s + 1;
+                break;
+            }
+        }
+        if(thirdTerm>1 and s==0){
+            cout << thirdTerm << "\n";
+        }
+        firstTerm = secondTerm;
+        secondTerm = thirdTerm;
+    }
+    return 0;
+}
