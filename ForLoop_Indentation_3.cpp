3be3334b0040cc633552c4d2eba7a08af3b302d1
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-07-21 14:24:25 +0530

    Add files via upload

---
+++ ForLoop_Indentation_3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int range;
+    cout<<"Enter a range:" <<"\n";
+    cin>>range;
+    for(int i=1;i<=range;i++)
+        cout<<i<<"\n";
+    
+
+    return 0;
+}
---
+++ WhileLoop_Indentation_4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int range;
+    cout<<"Enter a range:" <<"\n";
+    cin>>range;
+    int i =1;
+    while(i<range&&i++)
+        cout<<i<<"\n";//Indentation 
+        
+    
+    
+
+    return 0;
+}
